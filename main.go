e3a364778b46e025d10cacedaaa451fcbe0b2d83
Marcin <github@marcin.io>
2024-01-25 14:20:32 -0800

    fix timeout when using large mod profiles

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"path"
 	"time"
@@ -17,6 +16,7 @@ import (
 )
 
 type flags struct {
+	runTimeout                time.Duration
 	installDir                string
 	profileZip                string
 	workDir                   string
@@ -40,6 +40,7 @@ func init() {
 	flag.Usage = Usage
 
 	options = flags{}
+	flag.DurationVar(&options.runTimeout, "run-timeout", 5*time.Minute, "Total maximum runtime before giving up.")
 	flag.StringVar(&options.installDir, "install-dir", "", "Installation directory of the server.")
 	flag.StringVar(&options.profileZip, "profile-zip", "", "Profile export to apply.")
 	flag.StringVar(&options.workDir, "work-dir", "tmp/", "Temporary work directory for downloaded files.")
@@ -85,15 +86,110 @@ func main() {
 			ThunderstoreForceDownload: options.thunderstoreForceDownload,
 		}),
 		zip.Module(zip.Config{}),
-		fx.Invoke(run),
+		fx.Invoke(registerLifecycleHooks),
 	)
 
-	rand.Seed(time.Now().UnixNano())
 	app := fx.New(fxOptions...)
-	app.Run()
+
+	// start
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := app.Start(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// wait for app to finish
+	signal := <-app.Wait()
+
+	// stop
+	stopCtx, stopCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer stopCancel()
+	err = app.Stop(stopCtx)
+	if err != nil {
+		panic(err)
+	}
+
+	//exit with signal
+	os.Exit(signal.ExitCode)
 }
 
 func run(
+	ctx context.Context,
+	parser r2modman.ExportParser,
+	modutil r2modman.ModUtil,
+	extractor zip.Extractor,
+) error {
+	log.Println("Using profile", options.profileZip)
+	metadata, err := parser.Parse(options.profileZip)
+	if err != nil {
+		return err
+	}
+
+	packages, err := r2modman.GetPackagesMetadata(ctx)
+	if err != nil {
+		log.Printf("unable to pull thunderstore api: %s", err)
+		return err
+	}
+
+	log.Printf("Found packages %d packages from thunderstore\n", len(packages))
+
+	for _, v := range metadata.Mods {
+
+		log.Printf("processing %s\n", v.Name)
+
+		downloadedZipPath := path.Join(options.workDir, v.Filename())
+
+		thunderstoreMeta, ok := packages[v.ThunderstoreKey()]
+		if !ok {
+			return fmt.Errorf("thunderstore metadata does not exist for: %s", v.ThunderstoreKey())
+		}
+
+		err = modutil.Download(v, thunderstoreMeta)
+		if err != nil {
+			return err
+		}
+
+		//extract modes to install directory
+		packagingType, prefixToStrip, err := r2modman.DeterminePackagingType(downloadedZipPath)
+		if err != nil {
+			return err
+		}
+
+		installDir := fmt.Sprintf("%s/%s", options.installDir, packagingType.Directory())
+
+		if packagingType == r2modman.ModPackagingTypePlugin {
+			installDir += fmt.Sprintf("/plugins/%s", v.Name)
+		}
+
+		if packagingType == r2modman.ModPackagingTypeRootDLL {
+			installDir += fmt.Sprintf("/%s", v.Name)
+		}
+
+		log.Printf("Packaging Type: %s", packagingType)
+		log.Printf("Install Dir: %s", installDir)
+		log.Printf("Prefix Strip: %s", prefixToStrip)
+		err = extractor.Extract(downloadedZipPath, installDir, prefixToStrip)
+		if err != nil {
+			return err
+		}
+
+		log.Printf("")
+	}
+
+	// extract profile to bepinex in install dir
+	bepinDir := path.Join(options.installDir, "/BepInEx")
+	err = extractor.Extract(options.profileZip, bepinDir, "BepInEx")
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Mod install finished successfully, configure your start script at %s/start_server_bepinex.sh\n", options.installDir)
+
+	return nil
+}
+
+func registerLifecycleHooks(
 	lc fx.Lifecycle,
 	shutdowner fx.Shutdowner,
 
@@ -101,75 +197,29 @@ func run(
 	modutil r2modman.ModUtil,
 	extractor zip.Extractor,
 ) {
+
 	lc.Append(fx.Hook{OnStart: func(c context.Context) error {
 
-		log.Println("Using profile", options.profileZip)
-		metadata, err := parser.Parse(options.profileZip)
-		if err != nil {
-			return err
-		}
+		errorChannel := make(chan error)
 
-		packages, err := r2modman.GetPackagesMetadata(c)
-		if err != nil {
-			log.Printf("unable to pull thunderstore api: %s", err)
-			return err
-		}
+		go func(returnChannel chan<- error) {
+			myCtx, myCtxCancel := context.WithTimeout(context.Background(), options.runTimeout)
+			defer myCtxCancel()
+			returnChannel <- run(myCtx, parser, modutil, extractor)
 
-		log.Printf("Found packages %d packages from thunderstore\n", len(packages))
+		}(errorChannel)
 
-		for _, v := range metadata.Mods {
-
-			log.Printf("processing %s\n", v.Name)
-
-			downloadedZipPath := path.Join(options.workDir, v.Filename())
-
-			thunderstoreMeta, ok := packages[v.ThunderstoreKey()]
-			if !ok {
-				return fmt.Errorf("thunderstore metadata does not exist for: %s", v.ThunderstoreKey())
-			}
-
-			err = modutil.Download(v, thunderstoreMeta)
+		go func(errChan chan error, shutdowner fx.Shutdowner) {
+			err := <-errChan
 			if err != nil {
-				return err
+				shutdowner.Shutdown(fx.ExitCode(1))
+				return
 			}
+			shutdowner.Shutdown(fx.ExitCode(0))
+		}(errorChannel, shutdowner)
 
-			//extract modes to install directory
-			packagingType, prefixToStrip, err := r2modman.DeterminePackagingType(downloadedZipPath)
-			if err != nil {
-				return err
-			}
-
-			installDir := fmt.Sprintf("%s/%s", options.installDir, packagingType.Directory())
-
-			if packagingType == r2modman.ModPackagingTypePlugin {
-				installDir += fmt.Sprintf("/plugins/%s", v.Name)
-			}
-
-			if packagingType == r2modman.ModPackagingTypeRootDLL {
-				installDir += fmt.Sprintf("/%s", v.Name)
-			}
-
-			log.Printf("Packaging Type: %s", packagingType)
-			log.Printf("Install Dir: %s", installDir)
-			log.Printf("Prefix Strip: %s", prefixToStrip)
-			err = extractor.Extract(downloadedZipPath, installDir, prefixToStrip)
-			if err != nil {
-				return err
-			}
-
-			log.Printf("")
-		}
-
-		// extract profile to bepinex in install dir
-		bepinDir := path.Join(options.installDir, "/BepInEx")
-		err = extractor.Extract(options.profileZip, bepinDir, "BepInEx")
-		if err != nil {
-			return err
-		}
-
-		log.Printf("Mod install finished successfully, configure your start script at %s/start_server_bepinex.sh\n", options.installDir)
-
-		return shutdowner.Shutdown()
+		return nil
 
 	}})
+
 }
